b06bd5c9aab41cda0dfc77ef6a098d807346277d
Macylion <macylion@hotmail.com>
2019-12-14 12:42:24 +0100

    debugCamera

--- core/src/com/github/macylion/Minecraft2D.java
+++ core/src/com/github/macylion/Minecraft2D.java
@@ -2,10 +2,12 @@ package com.github.macylion;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.github.macylion.texturebank.TextureBank;
 
 public class Minecraft2D extends ApplicationAdapter {
@@ -14,6 +16,8 @@ public class Minecraft2D extends ApplicationAdapter {
 	SpriteBatch batch;
 	TextureBank txtBank;
 	OrthographicCamera cam;
+	//debug
+	Vector2 debugPos;
 	
 	@Override
 	public void create () {
@@ -21,19 +25,36 @@ public class Minecraft2D extends ApplicationAdapter {
 		txtBank = new TextureBank();
 		txtBank.addTexture("badlogic.jpg", "badlogic");
 		cam = new OrthographicCamera(WIDTH, HEIGHT);
+		//debug
+		debugPos = new Vector2(0, 0);
 	}
 
 	@Override
 	public void render () {
-		cam.update();
-		batch.setProjectionMatrix(cam.combined);
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		update();
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(txtBank.getTexture("badlogic"), 0, 1);
 		batch.end();
 	}
 	
+	private void update() {
+		cam.update();
+		batch.setProjectionMatrix(cam.combined);
+		//debug
+		float debugSpeed = 250;
+		if(Gdx.input.isKeyPressed(Keys.NUMPAD_6))
+			debugPos.x += debugSpeed * Gdx.graphics.getDeltaTime();
+		if(Gdx.input.isKeyPressed(Keys.NUMPAD_4))
+			debugPos.x -= debugSpeed * Gdx.graphics.getDeltaTime();
+		if(Gdx.input.isKeyPressed(Keys.NUMPAD_8))
+			debugPos.y += debugSpeed * Gdx.graphics.getDeltaTime();
+		if(Gdx.input.isKeyPressed(Keys.NUMPAD_2))
+			debugPos.y -= debugSpeed * Gdx.graphics.getDeltaTime();
+		cam.position.set(debugPos, 0);
+	}
+
 	@Override
 	public void dispose () {
 		batch.dispose();
