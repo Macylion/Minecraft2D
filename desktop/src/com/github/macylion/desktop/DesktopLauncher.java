1b5068b759da325ae26796985fc2df8078e652f0
Macylion <macylion@hotmail.com>
2019-12-14 11:53:44 +0100

    First commit

---
+++ desktop/src/com/github/macylion/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package com.github.macylion.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.github.macylion.Minecraft2D;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Minecraft 2D";
+		new LwjglApplication(new Minecraft2D(), config);
+	}
+}
